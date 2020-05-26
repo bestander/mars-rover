83c5c7c3793ec5d73f09d80dff580c5734a642a7
Konstantin Raev <bestander@gmail.com>
2020-05-26 01:01:13 -0700

    simplified

--- main.go
+++ main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"time"
 
-	"github.com/op/go-logging"
 	"github.com/stianeikeland/go-rpio"
 )
 
-func init() {
-	stderrorLog := logging.NewLogBackend(os.Stderr, "", 0)
-	stderrorLogLeveled := logging.AddModuleLevel(stderrorLog)
-	stderrorLogLeveled.SetLevel(logging.INFO, "")
-	logging.SetBackend(stderrorLogLeveled)
-}
-
-func main() {
-
-	var mainLog = logging.MustGetLogger("Rover engines")
-	mainLog.Info("Starting")
-
+func testMotors() {
 	err := rpio.Open()
 	if err != nil {
-		mainLog.Error(err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer rpio.Close()
@@ -42,4 +32,14 @@ func main() {
 	}
 	leftSide.DutyCycle(0, 20)
 	rightSide.DutyCycle(0, 20)
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		// testMotors()
+	})
+
+	http.ListenAndServe(":8080", nil)
 }

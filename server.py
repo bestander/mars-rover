dae7c641cbf21044e58e111d10acc7c3acce4e72
Konstantin Raev <bestander@gmail.com>
2020-11-26 00:57:35 +0000

    replaced previous solution with rtcbot

--- server.py
+++ server.py
@@ -1,26 +1,14 @@
+from aiohttp import web
 import asyncio
+import cv2
+from evdev import InputDevice, categorize, ecodes, list_devices
 import json
-import socket
-import time
-import imutils
+from rtcbot import RTCConnection, getRTCBotJS, PiCamera
 import RPi.GPIO as GPIO
 import threading
-import cv2
-import quart
-from quart import Quart
-from quart import render_template
-from quart import websocket
-from quart import Response
-from evdev import InputDevice, categorize, ecodes, list_devices
 
-MIME_TYPES = {
-    "html": "text/html",
-    "js": "text/javascript",
-    "css": "text/css"
-}
+
 MAX_AXIS_VALUE = 32767
-PORT = 8765
-
 PWM_FREQUENCY = 50
 STOP_DUTY_CYCLE = 7.5 # PWM value (7.5% of 20ms cycle) is between forward and backward, means stop
 DUTY_CYCLE_RANGE = 2.5 # 7.5% - 10% means forward, 5% - 7.5% means backward
@@ -40,82 +28,9 @@ right_pwm = GPIO.PWM(RIGHT_SIDE_PIN, PWM_FREQUENCY)
 right_pwm.start(0)
 right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE)
 
-app = Quart(__name__)
-
-def test_run_motors():
-    # init sequesnce
-    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-    time.sleep(3)
-    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE)
-    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE)
-
-# run motors and delay to allow camera to init
-# test_run_motors()
-
-def get_hostname():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    hostname = s.getsockname()[0]
-    s.close()
-    return hostname
-
-@app.route("/")
-async def index():
-    # return the rendered template
-    return await render_template("controls.html")
-
-class Camera:
-    last_frame = []
-
-    def __init__(self, source: int):
-        print("init")
-        self.video_source = source
-        self.cv2_cam = cv2.VideoCapture(self.video_source)
-        self.event = asyncio.Event()
-
-    def set_video_source(self, source):
-        self.video_source = source
-        self.cv2_cam = cv2.VideoCapture(self.video_source)
-
-    async def get_frame(self):
-        print("get frame")
-
-        await self.event.wait()
-        self.event.clear()
-        return Camera.last_frame
-
-    def frames(self):
-        if not self.cv2_cam.isOpened():
-            raise RuntimeError("Could not start camera.")
-
-        while True:
-            print("frame while")
-
-            # read current frame
-            _, frame = self.cv2_cam.read()
-
-            # encode as a jpeg image and return it
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame = cv2.GaussianBlur(frame, (7,7), 0)
-            frame = cv2.Canny(frame, 50, 50)
-
-            Camera.last_frame = [cv2.imencode(".jpg", frame)[1].tobytes(), frame]
-            self.event.set()
-            asyncio.sleep(1)
-            yield Camera.last_frame
-        self.cv2_cam.release()
-
-async def gen(c: Camera):
-    for frame in c.frames():
-        # d_frame = cv_processing.draw_debugs_jpegs(c.get_frame()[1])
-        yield (b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame[0] + b"\r\n")
-
-c_gen = gen(Camera(0))
-
-@app.route("/video_feed")
-def video_feed():
-    return Response(c_gen, mimetype="multipart/x-mixed-replace; boundary=frame")
+routes = web.RouteTableDef()
+camera = PiCamera()
+bwSubscription = asyncio.Queue()
 
 def debounce(wait):
     """ Decorator that will postpone a functions
@@ -134,64 +49,92 @@ def debounce(wait):
         return debounced
     return decorator
 
+
+frames = 0
+@camera.subscribe
+async def onFrame(frame):
+    global frames
+    frames = frames + 1
+    if frames % 5 == 0:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame, (3,3), 0)
+        frame = cv2.Canny(frame, 50, 50)
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        await bwSubscription.put(frame)
+
+@routes.get("/")
+async def index(request):
+    return web.FileResponse('index.html')
+
+@routes.get("/rtcbot.js")
+async def rtcbotjs(request):
+    return web.Response(content_type="application/javascript", text=getRTCBotJS())
+
+# This sets up the connection
+@routes.post("/connect")
+async def connect(request):
+    clientOffer = await request.json()
+    conn = RTCConnection()
+    conn.video.putSubscription(bwSubscription)
+    serverResponse = await conn.getLocalDescription(clientOffer)
+    conn.subscribe(onMessage)
+    return web.json_response(serverResponse)
+    
+
 @debounce(1)
 def stop():
    print("stopping")
    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE)
    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE)
 
-@app.websocket("/ws")
-async def handle_websocket_commands():
-    websocket.headers
-    while True:
-        print("handle_websocket_commands")
-        try:
-            message = await websocket.receive()
-            data = json.loads(message)
-            if data["action"] == "move":
-                direction = data["direction"]
-                print(f"< {direction}")
-                if direction == "forward":
-                    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-                    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
-                elif direction == "backward":
-                    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
-                    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-                elif direction == "left":
-                    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-                    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
-                elif direction == "right":
-                    left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
-                    right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
-                response = f"Ack {direction}!"
-                await websocket.send(response)
-                stop()
-            else:
-                print("unsupported event: {}", data)
-                await websocket.send(f"Echo {data}")
-        except asyncio.CancelledError:
-            # Handle disconnect
-            raise
+async def onMessage(data):
 
-async def wait_for_controller():
-    print("wait_for_controller")
+    if data["action"] == "move":
+        direction = data["direction"]
+        print(f"< {direction}")
+        if direction == "forward":
+            left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
+            right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
+        elif direction == "backward":
+            left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
+            right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
+        elif direction == "left":
+            left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
+            right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
+        elif direction == "right":
+            left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE)
+            right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE)
+        stop()
+    else:
+        print("unsupported event: {}", data)
+
+async def cleanup(app):
+    # TODO close all connections gracefully?
+    camera.close()
+
+app = web.Application()
+app.router.add_static('/static', './static')
+app.add_routes(routes)
+app.on_shutdown.append(cleanup)
+
+async def waitForController():
+    print("waitForController")
     devices = [InputDevice(path) for path in list_devices()]
     controller = next((device for device in devices if device.name == "Wireless Steam Controller"), None)
     if controller == None:
         await asyncio.sleep(1)
-        return (await wait_for_controller())
+        return (await waitForController())
     else:
-        return await controller_event_hanlder(controller)
+        return await onControllerEvent(controller)
 
 last_x_axis_value = 0
 last_y_axis_value = 0
 
-async def controller_event_hanlder(dev):
+async def onControllerEvent(dev):
     global last_x_axis_value
     global last_y_axis_value
 
     async for event in dev.async_read_loop():
-        print("controller_event_hanlder")
         if event.type == ecodes.EV_ABS:
             category = categorize(event)
             if (ecodes.ABS[event.code] == 'ABS_X'):
@@ -211,21 +154,6 @@ async def controller_event_hanlder(dev):
             left_pwm.ChangeDutyCycle(left_duty_cycle)
             right_pwm.ChangeDutyCycle(right_duty_cycle)
 
-if __name__ == "__main__":
-    hostname = get_hostname()
-    loop = asyncio.get_event_loop()
-   
-    try:
-        loop.create_task(wait_for_controller())
-        loop.run_until_complete(app.run_task(host = hostname, port = PORT))
-
-    except KeyboardInterrupt:
-        print("User Cancelled")
-
-    finally:
-        print("Cleaning")
-        left_pwm.stop()
-        right_pwm.stop()
-        GPIO.cleanup()
-        quit()
-   
+loop = asyncio.get_event_loop()
+loop.create_task(waitForController())
+web.run_app(app, port = 8000)

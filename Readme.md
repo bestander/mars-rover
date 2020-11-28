9b1180d8049bb3f47222fc390e90d4d56af7a36d
Konstantin Raev <bestander@gmail.com>
2020-11-27 16:04:14 -0800

    cleanup and readme

--- Readme.md
+++ Readme.md
@@ -1,11 +1,23 @@
-# Mars Rover
+# Profanity Mars Rover
 
+This is an R&D project of 3d printed rover powered by Raspberry Pi and controlled by either a camera over internet or a gamepad.
+Video can be streamed via browser, analyzed on Raspberry Pi or remotely in the cloud.
 
 ## Body 3D prints 
 
+Project is based on https://www.thingiverse.com/thing:1318414 (costs 10$ to obtain the STL files), the schematics and parts lists are incomplete, so it required some research to get all the parts together, I was able to obrain everything through Amazon and will list all the parts here later.
 
 ## Wire up
 
+2BD: I'll post pictures here
+
+## Code
+
+Rover controls are written in Python 3, main components:
+- USB reciever for gamepad controls (evdev package)
+- Pi Camera (RTCBot package)
+- Web server (aiohttp package) + React/WebRTC based client
+- Motors controls (RPi.GPIO package)
 
 
 ### Quick setup
@@ -14,12 +26,12 @@
 - Enable I2C and SSH
 - sudo apt install python3-rpi.gpio
 - python3 server.py
-- installing missing native .so files https://blog.piwheels.org/how-to-work-out-the-missing-dependencies-for-a-python-package/
+- Installing missing native .so files https://blog.piwheels.org/how-to-work-out-the-missing-dependencies-for-a-python-package/ (for cv2)
 
 ## Autostart
 
 ```/lib/systemd/system/mars-rover.service
-[Unit]
+ [Unit]
  Description=Mars Rover Service
  After=multi-user.target
 
@@ -34,5 +46,7 @@
  [Install]
  WantedBy=multi-user.target
  ```
-sudo systemctl stop mars-rover
+
+To test:
+`sudo systemctl start mars-rover`
 
--- server.py
+++ server.py
@@ -8,8 +8,9 @@ import RPi.GPIO as GPIO
 
 MAX_AXIS_VALUE = 32767
 PWM_FREQUENCY = 50
-STOP_DUTY_CYCLE = 7.5 # PWM value (7.5% of 20ms cycle) is between forward and backward, means stop
-DUTY_CYCLE_RANGE = 2.5 # 7.5% - 10% means forward, 5% - 7.5% means backward
+# PWM value (7.5% of 20ms cycle) is between forward and backward, means stop
+STOP_DUTY_CYCLE = 7.5
+DUTY_CYCLE_RANGE = 2.5  # 7.5% - 10% means forward, 5% - 7.5% means backward
 
 LEFT_SIDE_PIN = 35
 RIGHT_SIDE_PIN = 33
@@ -31,24 +32,29 @@ camera = PiCamera()
 bwSubscription = asyncio.Queue()
 
 frames = 0
+
+
 @camera.subscribe
 async def onFrame(frame):
     global frames
     frames = frames + 1
     if frames % 5 == 0:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.GaussianBlur(frame, (3,3), 0)
+        frame = cv2.GaussianBlur(frame, (3, 3), 0)
         frame = cv2.Canny(frame, 50, 50)
         frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
         await bwSubscription.put(frame)
+
 
 @routes.get("/")
 async def index(request):
     return web.FileResponse('index.html')
 
+
 @routes.get("/rtcbot.js")
 async def rtcbotjs(request):
     return web.Response(content_type="application/javascript", text=getRTCBotJS())
+
 
 @routes.post("/connect")
 async def connect(request):
@@ -58,7 +64,8 @@ async def connect(request):
     serverResponse = await conn.getLocalDescription(clientOffer)
     conn.subscribe(onMessage)
     return web.json_response(serverResponse)
-    
+
+
 async def onMessage(data):
     if data["action"] == "move":
         direction = data["direction"]
@@ -80,6 +87,7 @@ async def onMessage(data):
     else:
         print("unsupported event: {}", data)
 
+
 async def cleanup(app):
     # TODO close all connections gracefully?
     camera.close()
@@ -89,9 +97,11 @@ app.router.add_static('/static', './static')
 app.add_routes(routes)
 app.on_shutdown.append(cleanup)
 
+
 async def waitForController():
     devices = [InputDevice(path) for path in list_devices()]
-    controller = next((device for device in devices if device.name == "Wireless Steam Controller"), None)
+    controller = next((device for device in devices if device.name ==
+                       "Wireless Steam Controller"), None)
     if controller == None:
         await asyncio.sleep(1)
         return (await waitForController())
@@ -101,37 +111,43 @@ async def waitForController():
 last_x_axis_value = 0
 last_y_axis_value = 0
 
+
 async def onControllerEvent(dev):
     global last_x_axis_value
     global last_y_axis_value
 
     async for event in dev.async_read_loop():
         if event.type == ecodes.EV_ABS:
-            category = categorize(event)
             if (ecodes.ABS[event.code] == 'ABS_X'):
                 last_x_axis_value = event.value
             if (ecodes.ABS[event.code] == 'ABS_Y'):
                 last_y_axis_value = event.value
-            
+
             if abs(last_x_axis_value) > abs(last_y_axis_value):
                 # turn mode
-                left_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE * last_x_axis_value / MAX_AXIS_VALUE)
-                right_pwm.ChangeDutyCycle(STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * last_x_axis_value / MAX_AXIS_VALUE)
+                left_pwm.ChangeDutyCycle(
+                    STOP_DUTY_CYCLE + DUTY_CYCLE_RANGE * last_x_axis_value / MAX_AXIS_VALUE)
+                right_pwm.ChangeDutyCycle(
+                    STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * last_x_axis_value / MAX_AXIS_VALUE)
             else:
                 # drive mode
                 left_adjustment = 1
                 right_adjustment = 1
                 if last_x_axis_value > 0:
-                    left_adjustment = (1 - abs(last_x_axis_value) / MAX_AXIS_VALUE)
+                    left_adjustment = (
+                        1 - abs(last_x_axis_value) / MAX_AXIS_VALUE)
                 else:
-                    right_adjustment = (1 - abs(last_x_axis_value) / MAX_AXIS_VALUE)
+                    right_adjustment = (
+                        1 - abs(last_x_axis_value) / MAX_AXIS_VALUE)
 
-                right_duty_cycle = STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * last_y_axis_value / MAX_AXIS_VALUE * left_adjustment
-                left_duty_cycle = STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * last_y_axis_value / MAX_AXIS_VALUE * right_adjustment
+                right_duty_cycle = STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * \
+                    last_y_axis_value / MAX_AXIS_VALUE * left_adjustment
+                left_duty_cycle = STOP_DUTY_CYCLE - DUTY_CYCLE_RANGE * \
+                    last_y_axis_value / MAX_AXIS_VALUE * right_adjustment
                 left_pwm.ChangeDutyCycle(left_duty_cycle)
                 right_pwm.ChangeDutyCycle(right_duty_cycle)
 
 loop = asyncio.get_event_loop()
 loop.create_task(waitForController())
 
-web.run_app(app, port = 8000)
+web.run_app(app, port=8000)

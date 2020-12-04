213aa1dd69eba4ba2256838ed3a2bd9e09f44a1c
Konstantin Raev <bestander@gmail.com>
2020-12-03 22:16:13 -0800

    hsv controls for video

--- static/index.js
+++ static/index.js
@@ -2,6 +2,13 @@ const styles = {
   container: {
     height: "100%",
     width: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  videoContainer: {
+    position: "relative",
+    height: "90%",
+    width: "100%",
   },
   overlay: {
     position: "absolute",
@@ -49,7 +56,7 @@ function Controls() {
     } else if (e.clientY > document.body.clientHeight * 2 / 3) {
       connection.put_nowait(
         JSON.stringify({ action: "move", direction: "backward" })
-      ) 
+      )
     } else {
       if (e.clientX < document.body.clientWidth / 3) {
         connection.put_nowait(
@@ -94,29 +101,128 @@ function Controls() {
     setConnection(newConnection);
   }, [])
 
-  return (<div style={styles.container}>
-    <video ref={refVideo} style={styles.video} webkit-playsinline="true" playsinline="true" />
-    <div style={styles.overlay}
-      onContextMenu={(e) => {
-          e.preventDefault();
-      }}
-      onClick={() => {
-        playVideo();
-      }}
-      onMouseDown={(e) => {
-        pointerDown(e)
-      }}
-      onPointerDown={(e) => {
-        pointerDown(e)
-      }}
-      onMouseUp={() => {
-        pointerUp()
-      }}
-      onPointerUp={() => {
-        pointerUp()
-      }}
-    >
-      {message}
+  return (
+    <div style={styles.container}>
+      <div style={styles.videoContainer}>
+        <video ref={refVideo} style={styles.video} webkit-playsinline="true" playsinline="true" />
+        <div style={styles.overlay}
+          onContextMenu={(e) => {
+            e.preventDefault();
+          }}
+          onClick={() => {
+            playVideo();
+          }}
+          onMouseDown={(e) => {
+            pointerDown(e)
+          }}
+          onPointerDown={(e) => {
+            pointerDown(e)
+          }}
+          onMouseUp={() => {
+            pointerUp()
+          }}
+          onPointerUp={() => {
+            pointerUp()
+          }}
+        >
+          {message}
+        </div>
+      </div>
+      <HSLControl connection={connection}></HSLControl>
+    </div>
+  )
+}
+
+const hslStyles = {
+  container: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  row: {
+  },
+}
+
+function HSLControl(props) {
+  const [hsv, setHSV] = React.useState({
+    hueMin: 0,
+    hueMax: 255,
+    satMin: 0,
+    satMax: 180,
+    valMin: 0,
+    valMax: 255,
+  });
+
+  React.useEffect(() => {
+    if (props.connection) {
+      props.connection.put_nowait(
+        JSON.stringify({ action: "hsv", hsv })
+      )
+    }
+  }, [hsv])
+
+  return (<div style={hslStyles.container}>
+    <div style={hslStyles.row}>
+      Hue
+    <input type="range"
+        min={0}
+        max={hsv.hueMax}
+        step={1}
+        defaultValue={hsv.hueMin}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, hueMin: +e.target.value });
+        }}
+      ></input><span>{hsv.hueMin}</span>
+      <input type="range"
+        min={hsv.hueMin}
+        max={255}
+        step={1}
+        defaultValue={hsv.hueMax}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, hueMax: +e.target.value });
+        }}
+      ></input><span>{hsv.hueMax}</span>
+    </div>
+    <div style={hslStyles.row}>
+      Saturation
+    <input type="range"
+        min={0}
+        max={hsv.satMax}
+        step={1}
+        defaultValue={hsv.satMin}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, satMin: +e.target.value });
+        }}
+      ></input><span>{hsv.satMin}</span>
+      <input type="range"
+        min={hsv.satMin}
+        max={180}
+        step={1}
+        defaultValue={hsv.satMax}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, satMax: +e.target.value });
+        }}
+      ></input><span>{hsv.satMax}</span>
+    </div>
+    <div style={hslStyles.row}>
+      Value
+    <input type="range"
+        min={0}
+        max={hsv.valMax}
+        step={1}
+        defaultValue={hsv.valMin}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, valMin: +e.target.value });
+        }}
+      ></input><span>{hsv.valMin}</span>
+      <input type="range"
+        min={hsv.valMin}
+        max={180}
+        step={1}
+        defaultValue={hsv.valMax}
+        onMouseUp={(e) => {
+          setHSV({ ...hsv, valMax: +e.target.value });
+        }}
+      ></input><span>{hsv.valMax}</span>
     </div>
   </div>)
 }
